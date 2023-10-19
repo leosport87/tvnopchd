a49b5029a7f30c237912273e44baf5209a3f68c2
leosport87 <122590882+leosport87@users.noreply.github.com>
2023-10-19 11:33:41 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="alternate" media="handheld" href="alternate_page.htm" />
+<meta name="generator" content="HTML Tidy for Linux (vers 25 March 2009), see www.w3.org" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Language" content="pt-br" />
+<meta name="robots" content="index, follow"/>
+<title>TVNOPCHD - Assistir tv online gratis ao vivo - futebol ao vivo - ver tv no pc hd</title>
+<meta name="description" content="TV Online, Assistir TV Online, Ver TV Online, TV no PC, TV ao Vivo, Futebol ao Vivo Gratis, TV Gratis, Assistir TV, Ver TV, Assista TV Gratis, TV on line, Assistir tv online gratis futebol ao vivo sbt record redetv campeonatos de futebol e varios outros canais brasileiros sem pagar nada por isso!"/>
+<meta name="keywords" content="tv online, tv online gratis, assistir tv online, assistir tv online gratis, ver tv online, ver tv online gratis, tv ao vivo, tv gratis, tv on line, tv no pc, ver tv, assistir tv, net tv, futebol online, futebol ao vivo gratis, assista tv online, assistir tv gratis, futebol ao vivo, tv gratis online, tv online gratis ao vivo pela internet, assistir tv no pc, tv, televisao, internet, canais, brasil, portugues, tuga,sbt, record, radio web, esporte, filmes, futebol, campeonato, tv online gratis, tv no pc, sportaovivo, supercanais,clips"/>
+<meta name="rating" content="General"/>
+        <meta name="Rating" content="General" />
+        <meta name="Distribution" content="Global" />
+        <meta name="copyright" content="Grupo Tvnopchd" />
+        <meta name="author" content="Leonardo" />
+        <meta name="google-site-verification" content="NTutAh_Ib4m8IZZR6qBcN-afOEdKoMJXGqInmwH8Ukc" />
+        <meta name="email" content="tvnopchd@hotmail.com" />
+        <meta name="revisit-after" content="1 day" />
+        <link rel="canonical" href="http://www.tvnopchd.tv" />
+        <link rel="shortcut icon" href="http://www.tvnopchd.tv/favicon.ico"/>
+        <meta name='adbooth' content='57f72dd7a5fa6e8db8089f788acae96a8150a411' />
+
+<style type="text/css">
+body {
+	background-image: url(imagens/escheresque_ste.png);
+}
+.bgcanais {
+	background-image: url(imagens/bg_grade.jpg);
+}
+.fundoparceiros {
+	background-image: url(imagens/hbg.gif);
+}
+.fonteparceiros {
+	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 14px;
+	font-style: normal;
+	font-weight: bold;
+	color: #06F;
+	text-decoration: none;
+	font-variant: normal;
+	text-transform: none;
+}
+</style>
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+.fonte1 {	font-size: 11px;
+	font-weight: normal;
+	color: #666;
+	font-family: Arial, Helvetica, sans-serif;
+}
+.fonte2 {	font-family: Impact;
+	font-size: 26px;
+	font-weight: normal;
+	color: #666;
+	text-decoration: none;
+	font-style: normal;
+	font-variant: normal;
+}
+.fonte3 {	font-family: Impact;
+	font-size: 23px;
+	font-weight: lighter;
+	color: #838383;
+	text-decoration: none;
+}
+.fonte4 {	font-family: CordiaUPC;
+	font-weight: normal;
+	font-size: 20px;
+}
+a:link {
+	color: #06F;
+	text-decoration: none;
+}
+a:hover {
+	color: #D6D6D6;
+	text-decoration: underline;
+}
+a:visited {
+	text-decoration: none;
+	color: #06F;
+}
+a:active {
+	text-decoration: none;
+}
+#apDiv1 {
+	position:absolute;
+	left:305px;
+	top:81px;
+	width:537px;
+	height:304px;
+	z-index:1;
+}
+</style>
+</head>
+
+<body>
+
+
+<table width="1048" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="1048" class="fundorows1"><table width="1044" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td align="left"><a href="http://tvnopchd.tv/" target="_new"><img src="banners/banner7.png" width="260" height="90" border="0" /></a></td>
+        <td width="728" align="center"><a href="http://tvnopchd.tv/" target="_blank"><img src="imagens/TV4.gif" width="728" height="90" border="0" /></a></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td height="42" align="center" bgcolor="#FFFFFF"><ul id="MenuBar1" class="MenuBarHorizontal">
+      <li><a href="http://tvnopchd.tv" target="_blank">INICIO</a>        </li>
+      <li><a href="prog2.html" target="Player">FUTEBOL AO VIVO</a></li>
+      <li><a href="ch/filmesonline.php" target="Player">FILMES ONLINE</a></li>
+      <li><a href="ch/seriesonline.php" target="Player">SERIES ONLINE</a></li>
+      <li><a href="ch/desenhos.php" target="Player">DESENHOS</a></li>
+      <li><a href="prog2.html" target="Player">ESPORTES AO VIVO</a>        </li>
+      <li><a href="mailto:tvnopchd@hotmail.com">CONTATO</a></li>
+    </ul></td>
+  </tr>
+  <tr>
+    <td height="239" align="center" class="bgcanais"><iframe src="http://tvnopchd.tv/menu1/menu.html" name="canais" width="970" height="200" scrolling="No" frameborder="0" id="canais"></iframe></td>
+  </tr>
+  <tr>
+    <td align="center"><table width="200" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td height="544" align="center" valign="top"><table width="200" border="0" cellpadding="0" cellspacing="0">
+          <tr>
+            <td width="79" align="center" bgcolor="#FFFFFF"><img src="imagens/t_tv.png" width="166" height="23" /></td>
+            <td width="16" align="center">&nbsp;</td>
+            <td width="83" align="center" bgcolor="#FFFFFF"><img src="imagens/t_xat.png" width="185" height="23" /></td>
+          </tr>
+          <tr>
+            <td height="521" align="center" valign="top" bgcolor="#FFFFFF"><iframe src="http://tvnopchd.tv/meio.html" name="Player" width="645" height="500" align="center" scrolling="yes" frameborder="0" id="Player" > </iframe>
+              &nbsp;</td>
+            <td align="center">&nbsp;</td>
+            <td align="center" valign="top" bgcolor="#FFFFFF"><embed src="http://www.xatech.com/web_gear/chat/chat.swf" quality="high" width=400" height="502" name="chat" FlashVars="id=217107280&GroupName=tvnopchd"" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://xat.com/update_flash.shtml" />        
+              &nbsp;</td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center"><table width="1046" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td width="500" align="center" bgcolor="#FFFFFF"><img src="imagens/t_facebook.png" width="128" height="23" /></td>
+        <td width="546" align="center" bgcolor="#FFFFFF"><img src="imagens/t_agenda.png" width="128" height="23" /></td>
+      </tr>
+      <tr>
+        <td align="center" valign="top" bgcolor="#FFFFFF"><iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FTV-NO-PC-HD-O-MELHOR-DA-TV-ONLINE-EM-HD%2F134515719980900&amp;width=490&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;border_color&amp;stream=false&amp;header=false" scrolling="No" frameborder="0" style="border:none; overflow:hidden; width:500px; height:400px;" allowtransparency="true"></iframe>&nbsp;</td>
+        <td align="center" valign="top" bgcolor="#FFFFFF"><iframe src="http://tvnopchd.tv/prog.php" name="Quadro_tv" marginheight="1" marginwidth="1" border="0" width="520" scrolling="auto" frameborder="0" height="400" target="Player"></iframe>&nbsp;</td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" valign="top"><table width="1048" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td width="1012" align="center" valign="top" bgcolor="#FFFFFF"><img src="imagens/t_filmes.png" width="128" height="23" /></td>
+      </tr>
+      <tr>
+        <td align="center" valign="top" bgcolor="#FFFFFF"><table width="200" border="0" cellpadding="0" cellspacing="0">
+          <tr>
+            <td align="center"><a href="http://www.filmeseseriesonline.gratis/uma-noite-de-crime-3/" target="Player"><img src="https://1.bp.blogspot.com/-Qw_9dC_6xe8/V443HT--jGI/AAAAAAAAE0s/6TiUYKv3grsogwJnAVgk_nRYxh6kZ7RCACLcB/s320/wqqS6Be8WhUL7NPjbH0h9v7wido.jpg" width="132" height="218" border="0" /></a></td>
+            <td align="center">&nbsp;</td>
+            <td align="center"><a href="http://www.filmeseseriesonline.gratis/nerds-bebidas-e-curticao/" target="Player"><img src="https://4.bp.blogspot.com/-utp03sxXtYI/WBCiEWhiU0I/AAAAAAAAGEI/NWOSQfyu9_c1r86yYVrlR2GXglN211aPQCLcB/s320/b48a7e7bc8.jpg" width="132" height="218" border="0" /></a></td>
+            <td align="center">&nbsp;</td>
+            <td align="center"><a href="http://www.filmeseseriesonline.gratis/festa-da-salsicha/" target="Player"><img src="https://1.bp.blogspot.com/-7_LMXfeAM5g/WBCWXA55orI/AAAAAAAAGDg/SuLqFGkwRV4R2k985Ss7I_X6lDvXelwmACLcB/s320/c6852ee86d.jpg" width="132" height="218" border="0" /></a></td>
+            <td align="center">&nbsp;</td>
+            <td align="center"><a href="http://www.filmeseseriesonline.gratis/o-homem-nas-trevas/" target="Player"><img src="https://4.bp.blogspot.com/-DsnPDHNDQL0/WAYZYQTgIGI/AAAAAAAAF-g/xO6GN2fZGLwMLN_4PBvIFJxS4YO3tPgFACLcB/s320/21d23f1cab.jpg" width="132" height="218" border="0" /></a></td>
+            <td align="center">&nbsp;</td>
+            <td align="center"><a href="http://www.filmeseseriesonline.gratis/mascots/" target="Player"><img src="https://2.bp.blogspot.com/-FJnRvUU2hSc/WAIg_TE0_0I/AAAAAAAAF9M/sDVLERclDvcyVFZnfSxIiUSHlO6QZ0dWQCLcB/s320/gzHm6hnjyyFhCSn36T4Ckr74Q75.jpg" width="132" height="218" border="0" /></a></td>
+            <td align="center">&nbsp;</td>
+            <td align="center"><a href="http://www.filmeseseriesonline.gratis/quando-as-luzes-se-apagam/" target="Player"><img src="https://1.bp.blogspot.com/-tCbk3SANCIo/V72hcAM4TXI/AAAAAAAAFUM/vEOzSQd27gAw9sdSyNUUUdvQ3ZuG0uOZACLcB/s1600/capa%2B%252813%2529.jpg" width="132" height="218" border="0" /></a></td>
+            <td align="center">&nbsp;</td>
+            <td align="center"><a href="http://www.filmeseseriesonline.gratis/assistir-truque-de-mestre-2-o-2o-ato-dublado/" target="Player"><img src="https://4.bp.blogspot.com/-ncv4w1hMYJU/V2F6qJETSnI/AAAAAAAAEI0/52lYEdWGHNEC6ULtl-tVE2zbpaYP-nwpgCLcB/s320/560738.jpg-r_1920_1080-f_jpg-q_x-xxyxx.jpg" width="132" height="218" border="0" /></a></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" valign="top" bgcolor="#FFFFFF">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" valign="top" class="fundoparceiros"><table width="200" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td colspan="16" align="center" valign="top"><p><img src="imagens/t_parceiros.png" width="137" height="23" /></p>
+          <table width="200" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td align="center">&nbsp;</td>
+            </tr>
+          </table></td>
+        </tr>
+      <tr>
+        <td align="center" valign="top"><img src="imagens/CineTV CS Servidor Cardsharing_1.png" width="125" height="125" /></td>
+        <td align="center">&nbsp;</td>
+        <td align="center"><a href="http://www.exoclick.com/?login=tvnopchd"><img src="http://www.exoclick.com/banners/120x60.gif" border="0"></a>&nbsp;</td>
+        <td align="center">&nbsp;</td>
+        <td align="center"><img src="imagens/pub.png" width="125" height="125" /></td>
+        <td align="center">&nbsp;</td>
+        <td align="center"><img src="imagens/pub.png" width="125" height="125" /></td>
+        <td align="center">&nbsp;</td>
+        <td align="center"><img src="imagens/pub.png" width="125" height="125" /></td>
+        <td align="center">&nbsp;</td>
+        <td align="center"><img src="imagens/pub.png" width="125" height="125" /></td>
+        <td align="center">&nbsp;</td>
+        <td align="center"><img src="imagens/pub.png" width="125" height="125" /></td>
+        <td align="center">&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" valign="top" class="fundoparceiros"><table width="938" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="137" align="center" class="fonteparceiros"><a href="http://www.cinetvcs.com.br/" target="_blank">Cine Tv CS</a></td>
+        <td width="159" align="center" class="fonteparceiros"><a href="http://www.exoclick.com/?login=tvnopchd" target="_blank">EXOCLICK</a></td>
+        <td width="266">&nbsp;</td>
+        <td width="193">&nbsp;</td>
+        <td width="183">&nbsp;</td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" valign="top" class="fundoparceiros"><div>
+      <pre><span class="fonte2">TVNOPCHD
+</span><span class="fonte3">ASSISTA TV ONLINE</span></pre>
+      <pre><span class="fonte3"><span class="fonte4">TVNOPCHD © 2010-2016 - Todos Direitos Reservados
+        A Cópia total ou parcial deste site é proibida.</span></span>
+<span class="fonte1">Não transmite nenhum desses streams, os canais aqui contidos já estão na internet e são colocados aqui através de códigos embed.
+A transmissão destes canais e respectiva   programação é da inteira responsabilidade das estações e/ou entidades   re-transmissoras.</span>
+
+<!-- Copyright (c)2009 Site Meter -->
+            <!-- INICIO CONTADOR -->
+<script type="text/javascript" src="http://widgets.amung.us/small.js"></script><script type="text/javascript">WAU_small('9l8vlwchwwop')</script>
+
+
+   <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,3647452,4,511,95,18,00000000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?3647452&101" alt="" border="0"></a></noscript>
+<!-- Histats.com  END  -->
+ </pre>
+    </div></td>
+  </tr>
+</table>
+
+<script type="text/javascript">
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"SpryAssets/SpryMenuBarDownHover.gif", imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
+</script>
+
+<style>
+#fblikepop {background-color: #fff;display: none;    position: fixed;    top: 200px;    _position: absolute;     width: 450px;    border: 10px solid #6F6F6F;    z-index: 200;-moz-border-radius: 9px;    -webkit-border-radius: 9px;    margin: 0pt;    padding: 0pt;    color: #333333;    text-align: left;font-family: arial,sans-serif;    font-size: 13px;} 
+#fblikepop body {background: #fff none repeat scroll 0%;    line-height: 1;    margin: 0pt;    height: 100%;} 
+.fbflush {cursor: pointer;    font-size: 11px !important;    color: #FFF !important;    text-decoration: none !important;    border: 0 !important;} #fblikebg {display: none;    position: fixed;_position: absolute;  height: 100%;    width: 100%;    top: 0;    left: 0;    background: #000000;    z-index: 100;} 
+#fblikepop #closeable {color: #333;float: right;    margin: 7px 0 0 0;} 
+#fblikepop h1 {    background: #6D84B4 none repeat scroll 0 0;    border-top: 1px solid #3B5998;    border-left: 1px solid #3B5998;    border-right: 1px solid #3B5998;    color: #FFFFFF !important;    font-size: 14px !important;    font-weight: normal !important;    padding: 5px !important;    margin: 0 !important;font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif !important;} 
+#fblikepop #actionHolder { height: 60px;    overflow: hidden;}
+#fblikepop #buttonArea { background: #F2F2F2;    border-top: 1px solid #CCCCCC;    padding: 10px;    min-height: 50px;} 
+#fblikepop #buttonArea a { color: #999999 !important;    text-decoration: none !important;    border: 0 !important;    font-size: 10px !important;} #fblikepop #buttonArea a:hover {    color: #333 !important;    text-decoration: none !important; border: 0 !important;} 
+#fblikepop #popupMessage {font-size: 12px !important;font-weight: normal !important;    line-height: 22px;    padding: 8px;    background: #fff !important;} 
+#fblikepop #counter-display {float: right;    font-size: 11px !important;font-weight: normal !important;    margin: 5px 0 0 0;text-align: right;line-height: 16px;}</style> 
+<script src="http://www.google.com/jsapi"></script>
+<script>google.load("jquery", "1");</script> 
+<script type="text/javascript" src="http://connect.facebook.net/pt_BR/all.js#xfbml=1"></script> 
+<script type="text/javascript">
+ //<![CDATA[   
+  kakinetworkdotcom01username="pages/TV-NO-PC-HD-O-MELHOR-DA-TV-ONLINE-EM-HD/134515719980900",
+  kakinetworkdotcom01title="Encontre-nos no Facebook",
+  kakinetworkdotcom01time="30",
+  kakinetworkdotcom01wait="0",
+  kakinetworkdotcom01lang="br"
+ //]]>  
+ </script> 
+ <script type="text/javascript" src="http://mundoblogger.webs.com/scripts/popupFBlikebox.js"></script>
+ <script type="text/javascript">
+   //<![CDATA[ 
+  $(document).ready(function(){$().kakinetworkdotcom({ closeable: true });});  
+   //]]>
+  </script>
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48169200-1', 'tvnopchd.tv');
+  ga('send', 'pageview');
+
+</script>
+
+
+<!-- 5advertise.com  stats  (hidden)-->
+<script type="text/javascript">document.write(unescape("%3Cscript src=%27http://s10.histats.com/js15.js%27 type=%27text/javascript%27%3E%3C/script%3E"));</script>
+<a href="http://www.histats.com" target="_blank" title="blog counter" ><script  type="text/javascript" >
+try {Histats.start(1,2837381,4,0,0,0,"");
+Histats.track_hits();} catch(err){};
+</script></a>
+<noscript><a href="http://www.histats.com" target="_blank"><img  src="http://sstatic1.histats.com/0.gif?2837381&101" alt="blog counter" border="0"></a></noscript>
+<!-- 5advertise.com  stats END  -->
+
+</body>
+</html>
